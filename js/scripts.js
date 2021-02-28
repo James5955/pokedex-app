009a2ce8843791486568b790b4366d9989f492f5
James D <78052504+James5955@users.noreply.github.com>
2021-02-27 21:50:56 -0800

    Corrected syntax errors ; to ,

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,43 @@
 let pokemonList = [{},{},{},{},{},{},{}];
 
 pokemonList[0] = {
-    name: 'Bulbasaur';
-    height: 0.7;
-    type: ['Grass', 'Poison'];
+    name: 'Bulbasaur',
+    height: 0.7,
+    type: ['Grass', 'Poison']
   };
 
 pokemonList[1] = {
-    name: 'Charmander';
-    height: 0.6;
-    type: 'Fire';
+    name: 'Charmander',
+    height: 0.6,
+    type: 'Fire'
   };
 
 pokemonList[2] = {
-    name: 'Squirtle';
-    height: 0.5;
-    type: 'Water';
+    name: 'Squirtle',
+    height: 0.5,
+    type: 'Water'
   };
 
 pokemonList[3] = {
-    name: 'Gastly';
-    height: 1.3;
-    type: ['Ghost', 'Poison'];
+    name: 'Gastly',
+    height: 1.3,
+    type: ['Ghost', 'Poison']
   };
 
 pokemonList[4] = {
-    name: 'Haunter';
-    height: 1.6;
-    type: ['Ghost', 'Poison'];
+    name: 'Haunter',
+    height: 1.6,
+    type: ['Ghost', 'Poison']
   };
 
 pokemonList[5] = {
-    name: 'Gengar';
-    height: 1.5;
-    type: ['Ghost', 'Poison'];
+    name: 'Gengar',
+    height: 1.5,
+    type: ['Ghost', 'Poison']
   };
 
 pokemonList[6] = {
-    name: 'Pikachu';
-    height: 0.4;
-    type: 'Electric';
+    name: 'Pikachu',
+    height: 0.4,
+    type: 'Electric'
   };
