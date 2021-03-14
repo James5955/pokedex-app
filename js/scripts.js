663cc0aab23ce1b429db380780eb9a1e6b953279
James5955 <78052504+James5955@users.noreply.github.com>
2021-03-13 18:47:41 -0800

    Created an IIFE for my pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,54 +1,58 @@
-let pokemonList = [];
+(function () {
+  // This is my IIFE for pokemonList
+  let pokemonList = [];
 
-pokemonList[0] = {
-    name: 'Bulbasaur',
-    height: 0.7,
-    type: ['Grass', ' Poison']
-  };
+  pokemonList[0] = {
+      name: 'Bulbasaur',
+      height: 0.7,
+      type: ['Grass', ' Poison']
+    };
 
-pokemonList[1] = {
-    name: 'Charmander',
-    height: 0.6,
-    type: ['Fire']
-  };
+  pokemonList[1] = {
+      name: 'Charmander',
+      height: 0.6,
+      type: ['Fire']
+    };
 
-pokemonList[2] = {
-    name: 'Squirtle',
-    height: 0.5,
-    type: ['Water']
-  };
+  pokemonList[2] = {
+      name: 'Squirtle',
+      height: 0.5,
+      type: ['Water']
+    };
 
-pokemonList[3] = {
-    name: 'Gastly',
-    height: 1.3,
-    type: ['Ghost', ' Poison']
-  };
+  pokemonList[3] = {
+      name: 'Gastly',
+      height: 1.3,
+      type: ['Ghost', ' Poison']
+    };
 
-pokemonList[4] = {
-    name: 'Haunter',
-    height: 1.6,
-    type: ['Ghost', ' Poison']
-  };
+  pokemonList[4] = {
+      name: 'Haunter',
+      height: 1.6,
+      type: ['Ghost', ' Poison']
+    };
 
-pokemonList[5] = {
-    name: 'Gengar',
-    height: 1.5,
-    type: ['Ghost', ' Poison']
-  };
+  pokemonList[5] = {
+      name: 'Gengar',
+      height: 1.5,
+      type: ['Ghost', ' Poison']
+    };
 
-pokemonList[6] = {
-    name: 'Pikachu',
-    height: 0.4,
-    type: ['Electric']
-  };
+  pokemonList[6] = {
+      name: 'Pikachu',
+      height: 0.4,
+      type: ['Electric']
+    };
 
-  function myPokemonDetailsLoop(pokemon){
-    console.log('name: ' + pokemon.name);
-    console.log('height: ' + pokemon.height);
-    console.log('type: ' + pokemon.type);
-  }
+    function myPokemonDetailsLoop(pokemon){
+      console.log('name: ' + pokemon.name);
+      console.log('height: ' + pokemon.height);
+      console.log('type: ' + pokemon.type);
+    }
 
-pokemonList.forEach(myPokemonDetailsLoop);
+  pokemonList.forEach(myPokemonDetailsLoop);
+    
+})();
 
 //for (;pokemonList[index];){
 //  pokemonName = pokemonList[index].name;
