167599e96ee1ce6cd33c0475d417062f2565a780
James5955 <78052504+James5955@users.noreply.github.com>
2021-03-19 17:48:31 -0700

    removed my list of pokemon and made the array empty, also auto indented

--- js/scripts.js
+++ js/scripts.js
@@ -1,88 +1,46 @@
 let pokemonRepository = (function () {
-  // This is my IIFE for pokemonList
-  let pokemonList = [];
+// This is my IIFE for pokemonList
+let pokemonList = [];
 
-  pokemonList[0] = {
-      name: 'Bulbasaur',
-      height: 0.7,
-      type: ['Grass', ' Poison']
-    };
+  function myPokemonDetailsLoop(pokemon){
+    console.log('name: ' + pokemon.name);
+    console.log('height: ' + pokemon.height);
+    console.log('type: ' + pokemon.type);
+  }
 
-  pokemonList[1] = {
-      name: 'Charmander',
-      height: 0.6,
-      type: ['Fire']
-    };
-
-  pokemonList[2] = {
-      name: 'Squirtle',
-      height: 0.5,
-      type: ['Water']
-    };
-
-  pokemonList[3] = {
-      name: 'Gastly',
-      height: 1.3,
-      type: ['Ghost', ' Poison']
-    };
-
-  pokemonList[4] = {
-      name: 'Haunter',
-      height: 1.6,
-      type: ['Ghost', ' Poison']
-    };
-
-  pokemonList[5] = {
-      name: 'Gengar',
-      height: 1.5,
-      type: ['Ghost', ' Poison']
-    };
-
-  pokemonList[6] = {
-      name: 'Pikachu',
-      height: 0.4,
-      type: ['Electric']
-    };
-
-    function myPokemonDetailsLoop(pokemon){
-      console.log('name: ' + pokemon.name);
-      console.log('height: ' + pokemon.height);
-      console.log('type: ' + pokemon.type);
-    }
-
-  pokemonList.forEach(myPokemonDetailsLoop);
+pokemonList.forEach(myPokemonDetailsLoop);
 
 function add(pokemonName, pokemonHeight, pokemonType){
-  pokemonList.push( { name: pokemonName, height: pokemonHeight, type: pokemonType});
+pokemonList.push( { name: pokemonName, height: pokemonHeight, type: pokemonType});
 }
 
-  function getAll(){
-    return pokemonList;
-  }
+function getAll(){
+  return pokemonList;
+}
 
-  function addListItem(pokemon){
-    let pokemonOrderList = document.querySelector('.pokemon-list');
-    let listItem = document.createElement('li');
-    let button = document.createElement('button');
-    button.innerText = pokemon.name;
-    button.classList.add('pokemonButton');
-    listItem.appendChild(button);
-    pokemonOrderList.appendChild(listItem);
-    button.addEventListener('click', function (event) {
-      showDetails(pokemon);
-    });
-  }
+function addListItem(pokemon){
+  let pokemonOrderList = document.querySelector('.pokemon-list');
+  let listItem = document.createElement('li');
+  let button = document.createElement('button');
+  button.innerText = pokemon.name;
+  button.classList.add('pokemonButton');
+  listItem.appendChild(button);
+  pokemonOrderList.appendChild(listItem);
+  button.addEventListener('click', function (event) {
+    showDetails(pokemon);
+  });
+}
 
-  function showDetails(pokemon){
-    console.log(pokemon);
-  }
+function showDetails(pokemon){
+  console.log(pokemon);
+}
 
-  return{
-    add: add,
-    getAll: getAll,
-    addListItem: addListItem,
-    showDetails: showDetails
-  };
+return{
+  add: add,
+  getAll: getAll,
+  addListItem: addListItem,
+  showDetails: showDetails
+};
 })();
 
 console.log(pokemonRepository.getAll());
