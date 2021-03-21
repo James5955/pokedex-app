51e047ebaf8e10a65c27cc37385c16aea4413985
James D <78052504+James5955@users.noreply.github.com>
2021-03-21 16:40:31 -0700

    Created validate email function

--- js/scripts.js
+++ js/scripts.js
@@ -80,13 +80,16 @@ pokemonRepository.loadList().then(function() {
 
 // Register form validation!
 
-/*(function (){
+(function (){
   let form = document.querySelector('#register-form');
   let emailInput = document.querySelector('#email');
   let passwordInput = document.querySelector('#password');
 
   function validateEmail(){
-    return false;
+    let value = emailInput.value;
+    let hasAtSign = value.indexOf('@') > -1;
+    let hasDot = value.indexOf('.') > -1;
+    return value && hasAtSign && hasDot;
   }
 
   function validatePassword(){
